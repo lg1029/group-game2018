4012b6b75fd389ed34505238308d3bbc62805d4a
Lauren Marie Grassano <laurenmariegrassano@Laurens-MacBook-Pro-2.local>
2018-10-22 23:59:18 -0400

    8th commit

---
+++ New Unity Project/Assets/Background/CompleteMvmntControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class CompleteMvmntControl : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * speed);
+
+        //freeze rotation
+        rb2d.freezeRotation = true;
+	}
+}
---
+++ New Unity Project/Assets/Sprites/interpick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interpick : MonoBehaviour {
+
+
+
+    public void DoInteraction()
+    {
+        //pick up and store in inventory
+        gameObject.SetActive(false);
+    }
+}
