27bd91537881c20f44d053908e9c7dca56d5bcdd
Blair Crutchfield <bc18b@my.fsu.edu>
2018-12-03 18:01:26 -0500

    Menu Completed selection FIxed

---
+++ Damocles 2.0/Assets/SLCTOR.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SLCTOR : MonoBehaviour
+{
+    public GameObject Outline; 
+   public void toggleselection()
+    
+
+    {
+        print("Toggle button");
+        //If enabled, disable
+        if(Outline.activeSelf)
+        {
+            Outline.SetActive(false);
+        }
+
+        else
+        {
+            GameObject[] SLC = GameObject.FindGameObjectsWithTag("Selector");
+            foreach(GameObject selector in SLC)
+            {
+                selector.SetActive(false);
+            }
+
+            Outline.SetActive(true);
+        }
+    }  
+}
