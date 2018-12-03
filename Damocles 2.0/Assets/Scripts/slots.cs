bb039d9e50dcba28137df7f9eb7fc4b566c88542
Blair Crutchfield <bc18b@my.fsu.edu>
2018-12-03 15:20:38 -0500

    update

---
+++ Damocles 2.0/Assets/Scripts/slots.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slots : MonoBehaviour 
+{
+    public void DropItem()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
+	
